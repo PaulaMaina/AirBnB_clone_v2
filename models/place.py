fdd2dd0a9463aa8221b8afaebeb681aebd59dbdd
PaulaMaina <kabura.mainap@gmail.com>
2024-04-17 14:32:38 +0300

    Debug

--- models/place.py
+++ models/place.py
@@ -3,10 +3,16 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                          primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
