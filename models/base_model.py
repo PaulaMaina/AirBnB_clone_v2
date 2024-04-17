e06a4e93ebcd0eca014038fc168648349bcc376e
PaulaMaina <kabura.mainap@gmail.com>
2024-04-17 19:44:31 +0300

    BaseModel debug

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,11 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -27,18 +27,21 @@ class BaseModel:
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs:
-                self.created_at = datetime.now()
+                self.created_at = datetime.utcnow()
             if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def __repr__(self):
+        """Returns the string representation"""
+        return self.__str__()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -49,7 +52,7 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = dict(self.__dict__)
-        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["__class__"] = type(self).__name__
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
@@ -58,4 +61,4 @@ class BaseModel:
 
     def delete(self):
         """Deletes object"""
-        storage.delete(self)
+        models.storage.delete(self)
