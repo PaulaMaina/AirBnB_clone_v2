f43c15947385aa536d5077a5ca9a454a14c15f09
PaulaMaina <kabura.mainap@gmail.com>
2024-04-17 14:49:26 +0300

    DBStorage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenity class"""
+    __tablename__ = "amnenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
