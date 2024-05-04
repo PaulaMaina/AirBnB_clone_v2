9102d8975aa6a00f60021c8d4cbf1848d6f156e8
PaulaMaina <kabura..mainap@gmail.com>
2024-05-04 21:03:10 +0300

    Setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# This script sets up web servers for the web static deployment
+
+# Install Nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+#Create folders and file content
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# Grant permissions
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { lias/data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
