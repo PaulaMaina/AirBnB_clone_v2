965cf4964bb349aa4d42716b5118962af56306a5
PaulaMaina <kaburamainap@gmail.com>
2024-05-06 20:05:41 +0300

    Deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,9 +2,23 @@
 """This script distibutes an archive to the web servers"""
 
 
-from fabric.api import env, run, put
-from os.path import exists
+from fabric.api import env, run, put, local
+from os.path import exists, isdir
+from datetime import datetime
 env.hosts = ['54.234.7.217', '18.235.233.205']
+
+
+def do_pack():
+    """Generates a .tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        filename = "versions/web_static/{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -24,5 +38,13 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
+
+
+def deploy():
+    """Creates and distributes an srchive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
