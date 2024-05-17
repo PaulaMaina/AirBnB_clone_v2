bf1c0253ad5454af241e0304e87ee958ed86e35f
PaulaMaina <kaburamainap@gmail.com>
2024-05-17 13:25:04 +0300

    First commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script starts a Flask web app"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Returns hello message"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
