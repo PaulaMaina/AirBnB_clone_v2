f1ec281ea3cdb5aff59b23a203b7895ebe3330c7
PaulaMaina <kaburamainap@gmail.com>
2024-05-21 18:41:08 +0300

    Python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,6 +24,7 @@ def c(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_is_cool(text="is cool"):
     """Display Python followed by some text"""
