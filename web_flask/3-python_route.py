ca5a7303ad752c5aca782dadfb093535f40e3dd2
PaulaMaina <kaburamainap@gmail.com>
2024-05-21 18:38:19 +0300

    Python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This script starts a Flask web app"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Returns hello message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Display C followed by some text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Display Python followed by some text"""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
