0a480df18c037a32b71f81993eedc61826efbc55
PaulaMaina <kabura.mainap@gmail.com>
2024-05-23 05:07:10 -0700

    Even or odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict _slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def evenorodd(n):
     """Displays a HTML page if an number is even or odd"""
     if n % 2 == 0:
